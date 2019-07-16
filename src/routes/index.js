64692ad71a445fb0def30f78770425aeaff5b7bf
Breno França <franciscobreno.si@gmail.com>
2019-07-16 12:59:29 -0300

    reestruturação das pastas

---
+++ src/routes/index.js
@@ -0,0 +1,7 @@
+import { createAppContainer, createSwitchNavigator } from "react-navigation";
+
+import SignIn from "~/pages/SignIn";
+
+const Routes = createAppContainer(createSwitchNavigator({ SignIn }));
+
+export default Routes;
